d69ec87ddaf441d0c7f479730c686c175b954b81
play.js <git@gituser.com>
2021-01-21 09:09:03 -0800

    Added object lookup tables test

--- Language Testing/script.js
+++ Language Testing/script.js
@@ -1,10 +1,33 @@
 
-
 function SetupLanguageTesting()
 {
   DefaultFnParams();
+  AddStatus();
+  TestObjectArrays();
 }
 
+/**********************************************
+***********************************************
+       Object Arrays used as lookup tables
+***********************************************
+**********************************************/
+function TestObjectArrays()
+{
+  let lookuptable=
+  [
+  {id:"KSAN",lat:32,lon:-117,junk:[[2],[3]]},
+  {id:"KLAX",lat:33,lon:-117,junk:[[1],[2]]}
+  ];
+  AddStatus("Lookup table\n"+JSON.stringify(lookuptable));
+  let ksan=lookuptable.filter(x=>x.id=="KSAN");
+  //print out the entire found object
+  AddStatus("found object...");
+  AddStatus("KSAN = "+JSON.stringify(ksan[0]));
+  //print out the individual pieces
+  AddStatus("using individual pieces...");
+  AddStatus("id="+ksan[0].id+" latitude="+ksan[0].lat+
+            " longitude="+ksan[0].lon+" junk="+ksan[0].junk);
+}
 /**********************************************
 ***********************************************
        Default function Parameters
@@ -25,6 +48,7 @@ function fnWithDefaultParams(p1,p2=" p2default ",p3=" p3default ")
 
 function AddStatus(str)
 {
+  if (str==undefined)str="";
   stat=document.getElementById("status");
   stat.value+="\n"+str;
 }
